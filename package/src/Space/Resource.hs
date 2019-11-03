b8936c454b47950285cbfef1f48155a6a0278e7e
Samuel Schlesinger <samuel@simspace.com>
2019-11-03 17:16:29 -0500

    Changed recipe type

--- package/src/Space/Resource.hs
+++ package/src/Space/Resource.hs
@@ -1,7 +1,6 @@
 module Space.Resource where
 
 import Data.Map (Map)
-import qualified Data.Map as Map
 import Data.Set (Set)
 --import qualified Data.Set as Set
 
@@ -13,9 +12,6 @@ data Machine = Power | Weapons | Shields
 
 data Skill = Scientist (Set Science) | Machinist (Set Machine) 
 
-type Recipe = (Map Resource Integer, Map Resource Integer)
-
-multiplyRecipe :: Integer -> Recipe -> Recipe
-multiplyRecipe m (from, to) = (Map.map (* m) from, Map.map (* m) to) 
+type Recipe = Map Resource Integer -> Map Resource Integer
 
 type Inventory = Map Resource Integer

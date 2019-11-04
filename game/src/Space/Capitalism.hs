059182d4fd851c62428733f0c69bfc3faa3e6352
Samuel Schlesinger <samuel@simspace.com>
2019-11-03 19:59:11 -0500

    Added a big TODO about how we might want to structure some of the next things which will be coming, such as buying and selling things on each planet and monitoring arbitrage opportunities

--- game/src/Space/Capitalism.hs
+++ game/src/Space/Capitalism.hs
@@ -23,6 +23,24 @@ main = do
   (charAddHandler, fireChar) <- newAddHandler
   (tickAddHandler, fireTick) <- newAddHandler
 
+  -- TODO: initialize a number of handlers here which will be events to
+  -- affect the various markets in the system. fire these events in some
+  -- clever way that makes the player feel like the arbitrage opportunities
+  -- keep disappearing on them, while generating new ones. ideally, as the
+  -- game goes on, the economy should get "tighter", meaning that arbitrage
+  -- opportunities are hard to come by.
+  --
+  -- what is an arbitrage opportunity? it is basically just a way that you
+  -- can buy and sell goods in order to make a profit. if you can buy good
+  -- A on planet X for 1 unit each and sell good A on planet Y for 2 units
+  -- each, then as long as the transportation costs are accounted for in
+  -- your profits you can likely make a profit off running good A from
+  -- planet X to planet Y. the game should detect that good A is being
+  -- bought a lot and increase the demand for good A, meaning that its
+  -- price should slowly rise to make this arbitrage opportunity less and
+  -- less lucrative. similarly, planet Y should have the supply increase
+  -- and thus the price decrease, coming at you from both angles. 
+
   (void . forkIO . forever) do
     fireTick ()
     threadDelay 1_000_000
